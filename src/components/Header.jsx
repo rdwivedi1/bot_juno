3d79531c256970814008b728d43d6bbb3e1c5182
rdwivedi1 <82491458+rdwivedi1@users.noreply.github.com>
2024-02-05 15:58:23 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Header() {
-  return <div className="header">&nbsp;React Chatbot UI</div>;
+  return <div className="header">&nbsp;Juno, Your Cool Event Guide</div>;
 }
