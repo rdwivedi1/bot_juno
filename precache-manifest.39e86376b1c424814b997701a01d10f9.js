6aa683d250c7f48eed8d6f387d3466626cf498f2
2731rahuld <82491458+2731rahuld@users.noreply.github.com>
2024-02-06 10:56:48 +0530

    Updates

---
+++ precache-manifest.39e86376b1c424814b997701a01d10f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9109982ef106c1545dfc5c2757a3b4d",
+    "url": "/bot_juno/index.html"
+  },
+  {
+    "revision": "01705f27a1fac84112d8",
+    "url": "/bot_juno/static/css/main.c6b54ed6.chunk.css"
+  },
+  {
+    "revision": "255aa3bb7dcbc8d8e4ee",
+    "url": "/bot_juno/static/js/2.37c8826f.chunk.js"
+  },
+  {
+    "revision": "01705f27a1fac84112d8",
+    "url": "/bot_juno/static/js/main.a8dbfad0.chunk.js"
+  },
+  {
+    "revision": "eb1f02a2cb1c6ea979fa",
+    "url": "/bot_juno/static/js/runtime~main.1fa9abab.js"
+  }
+]);
